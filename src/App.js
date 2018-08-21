1cdcb29984b4c3a218e089a6fc44076e1581e30a
Asa Smith <smith.asa.la@gmail.com>
2018-08-20 21:40:36 -0400

    into to props

--- src/App.js
+++ src/App.js
@@ -2,15 +2,13 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-const welcome = 'Welcome to React Asa'
-
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <Welcome />
+          <Welcome text='Welcome to React Props' />
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
@@ -22,8 +20,10 @@ class App extends Component {
 
 class Welcome extends Component {
   render() {
+
+    const { text } = this.props
     return (
-      <h1 className="App-title">{welcome}</h1>
+      <h1 className="App-title">{text}</h1>
     )
   }
 }
