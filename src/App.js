4297562aacc81c5d3888f2c0d5a3476e71854614
Asa Smith <smith.asa.la@gmail.com>
2018-08-21 21:41:25 -0400

    intro to state

--- src/App.js
+++ src/App.js
@@ -3,16 +3,38 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
+  // set state
+  state = {
+    toggle: true,
+  }
+
+  // function to toggle state
+  // used in onClick
+  toggle = () => {
+    this.setState({
+      toggle: !this.state.toggle,
+    })
+  }
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <Welcome text='Welcome to React Props' />
+          
+          {/* pass toggle as a prop to Welcom component */}
+          <Welcome text='Welcome to React Props' toggle= {this.state.toggle} />
+
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+
+        { this.state.toggle && 
+          <p>this should show/hide</p>
+        }
+
+        <button onClick={this.toggle}>toggle</button>
       </div>
     );
   }
@@ -21,7 +43,11 @@ class App extends Component {
 class Welcome extends Component {
   render() {
 
-    const { text } = this.props
+    // object destructuring
+    const { text, toggle } = this.props
+
+    // toggle value changes 
+    console.log(toggle)
     return (
       <h1 className="App-title">{text}</h1>
     )
