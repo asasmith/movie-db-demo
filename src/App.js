a515e03c9b44c8f2d84e7c80ca9968b047f24c99
Asa Smith <smith.asa.la@gmail.com>
2018-08-23 00:47:13 -0400

    iterating and child components

--- src/App.js
+++ src/App.js
@@ -2,57 +2,36 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import Movie from './Movie'
+
+const movies = [
+  {
+    id: 1,
+    title: 'Avengers',
+  },
+  {
+    id: 2,
+    title: 'Avengers Age of Ulton',
+  },
+  {
+    id: 3,
+    title: 'Avengers: Infinity War',
+  },
+]
+
 class App extends Component {
-
-  state = {
-    input: 'Hello'
-  }
-
-  updateInput = (e) => {
-    this.setState({
-      input: e.target.value,
-    })
-  }
-  submit = () => {
-    console.log(this.text.value)
-  }
   
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          
-          {/* pass toggle as a prop to Welcom component */}
-          <Welcome text='Welcome to React Props' />
-
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
 
-        {/* controlled input */}
-        <input type='text' onChange={this.updateInput} value={this.state.input} />
+        {movies.map(movie => <Movie key={movie.id} movie={movie} />)}
 
-        {/* uncontrolled input */}
-        <input type='text' ref={(input) => this.text = input} />
-
-        <button onClick={this.submit}>Show Value</button>
       </div>
     );
-  }
-}
-
-class Welcome extends Component {
-  render() {
-
-    // object destructuring
-    const { text } = this.props
-
-    // toggle value changes 
-    return (
-      <h1 className="App-title">{text}</h1>
-    )
   }
 }
 
