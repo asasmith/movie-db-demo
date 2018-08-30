a643a23ee669443a91f4adcaeaebca2782a3a205
Asa Smith <smith.asa.la@gmail.com>
2018-08-30 00:38:11 -0400

    add a switch route and use route params

---
+++ src/MoviesList.js
@@ -0,0 +1,43 @@
+/* eslint react/destructuring-assignment: 0 */
+import React, { Component } from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+} from 'react-router-dom';
+import logo from './logo.svg';
+import './App.css';
+
+import Movie from './Movie';
+
+const API_KEY = process.env.REACT_APP_MOVIE_DB_KEY;
+
+class MoviesList extends Component {
+  state = {
+    movies: [],
+  }
+
+  async componentDidMount() {
+    try {
+      const res = await fetch(`https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1`);
+      const movies = await res.json();
+      console.log(movies);
+
+      this.setState({
+        movies: movies.results,
+      });
+    } catch (e) {
+      console.log(e);
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        {this.state.movies.map(movie => <Movie key={movie.id} movie={movie} />)}
+      </div>
+    );
+  }
+}
+
+export default MoviesList;
